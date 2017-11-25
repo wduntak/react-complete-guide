f41d70b8b0216e53366367354d7bd170156fa3d9
Wangdu Duntak <wduntak@kobo.com>
2017-11-25 15:25:40 -0500

    Bind nameChangeHandler to each component

--- src/App.js
+++ src/App.js
@@ -13,20 +13,27 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons : [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 31 },
-        { name: "Noryang", age: 27 }  
-      ]
-    })
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = { 
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons });
   }
 
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons}); 
+    this.setState({ persons }); 
   }
 
   togglePersonsHandler = () => {
@@ -54,7 +61,8 @@ class App extends Component {
               name={person.name}
               click={() => this.deletePersonHandler(index)} 
               age={person.age}
-              key={person.id}/>
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
         })}     
         </div> 
       );
