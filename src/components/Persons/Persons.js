684e09458282b4c24f653e1c8e8f4ffd4a7ba0cf
Wangdu Duntak <wduntak@kobo.com>
2017-12-02 15:54:10 -0500

    Update Persons and Person component to stateful components

---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Person from './Person/Person';
+
+class Persons extends Component {
+    render () {
+        return this.props.persons.map(( person, index ) => {
+            return <Person 
+                name={person.name}
+                key={person.id}
+                click={() => this.props.clicked( index )} 
+                age={person.age}
+                changed={( event ) => this.props.changed( event, person.id )}/>
+        });
+    }
+}
+    
+export default Persons;
