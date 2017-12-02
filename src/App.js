f5fb11ed291255632eb12d45bdcd881933696cc9
Wangdu Duntak <wduntak@kobo.com>
2017-12-02 13:52:17 -0500

    Understanding error boundary with componentDidCatch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -49,12 +50,11 @@ class App extends Component {
       persons = (
         <div>
         {this.state.persons.map((person, index) => {
-          return <Person 
+          return <ErrorBoundary key={person.id}><Person 
               name={person.name}
               click={() => this.deletePersonHandler(index)} 
               age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)}/>
+              changed={(event) => this.nameChangedHandler(event, person.id)}/></ErrorBoundary>
         })}     
         </div> 
       );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,6 +2,11 @@ import React from 'react';
 import classes from './Person.css';
 
 const Person = ( props ) => {
+    const rnd = Math.random();
+
+    if( rnd > 0.7 ) {
+        throw new Error('Something went wrong');
+    }
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm a {props.name} and I am a {props.age} years old!</p>
