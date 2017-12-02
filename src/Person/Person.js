442d5d69bcdaa4fe59eabea3414c455a15b99ca8
Wangdu Duntak <wduntak@kobo.com>
2017-12-02 12:45:38 -0500

    npm eject and modify webpack settings to include css modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 const Person = ( props ) => {
     return (
-        <div className="Person">
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm a {props.name} and I am a {props.age} years old!</p>
             {props.children}
             <input type="text" onChange={props.changed} value={props.name} />
